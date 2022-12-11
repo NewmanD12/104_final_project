1fcb05156e060dde3e89a4e9d2da27160d92e880
Dakota Newman <dakota.newman@yahoo.com>
2022-12-11 18:30:14 -0500

    finished promotion function when pawn gets across the board

--- main.js
+++ main.js
@@ -22,6 +22,16 @@ let blackKingUrl = "https://www.clipartmax.com/png/small/58-581746_chess-pieces-
 
 let blackQueenUrl = "https://www.clipartmax.com/png/small/204-2042941_clipart-queen-chess-piece-vector.png"
 
+let blackPieces = ["https://www.clipartmax.com/png/small/42-427459_free-vector-pawn-chess-piece-clip-art-chess-pieces.png", 
+                    "https://www.clipartmax.com/png/small/159-1599934_chess-piece-black-rook-chess-piece.png", 
+                    "https://www.clipartmax.com/png/small/160-1603738_clipart-knight-chess-piece-clipart-best-clipart-best-knight-chess-piece-clipart.png",
+                    "https://www.clipartmax.com/png/small/124-1240246_chess-tile-queen-clipart-bishop-chess-piece.png",
+                    "https://www.clipartmax.com/png/small/58-581746_chess-pieces-clip-art.png",
+                    "https://www.clipartmax.com/png/small/204-2042941_clipart-queen-chess-piece-vector.png"]
+
+function imageTagFunction(url) {
+    return `<img src='${url}'>`
+}
 
 let row1Squares = document.querySelectorAll('#row-1 span')
 let row2Squares = document.querySelectorAll('#row-2 span')
@@ -31,6 +41,7 @@ let row5Squares = document.querySelectorAll('#row-5 span')
 let row6Squares = document.querySelectorAll('#row-6 span')
 let row7Squares = document.querySelectorAll('#row-7 span')
 let row8Squares = document.querySelectorAll('#row-8 span')
+let optionsBox = document.querySelector('#options')
 
 let pieceSelected = false
 let pieceToMove = ''
@@ -50,9 +61,7 @@ let rowsAndSquares = {
 
 function addClickEvent(rowOfSquares){
     for(let square of rowOfSquares){
-        // console.log(square)
         square.addEventListener('click', () =>{
-            // console.log(square)
             if(!pieceSelected){
                 pieceSelected = true
                 square.style.border = '2px solid gold'
@@ -60,8 +69,96 @@ function addClickEvent(rowOfSquares){
                 square.innerHTML = ''
                 tileMovedFrom = square.id
                 findPossibleMoves(square.id)
-                // console.log(square.id)
             } else {
+                let movedToTile = Number(square.id.split('-')[1])
+                let row = findRow(movedToTile)
+                // console.log(pieceToMove)
+                if(pieceToMove.includes(whitePawnUrl) && row === 1){
+                    // console.log('you made it!')
+                    let div = document.createElement('div')
+                    let sentence = document.createElement('div')
+                    let queenImage = document.createElement('img')
+                    let rookImage = document.createElement('img')
+                    let knightImage = document.createElement('img')
+
+                    div.classList.add('justify-content-center')
+                    sentence.classList.add('row')
+                    sentence.classList.add('justify-content-center')
+                    sentence.innerText = 'Select a piece to upgrade your pawn.'
+                    queenImage.addEventListener('click', () =>{
+                        let image = imageTagFunction(whiteQueenUrl)
+                        square.innerHTML = image
+                        div.remove()
+                    })
+                    rookImage.addEventListener('click', () =>{
+                        console.log('you clicked the rook')
+                        let image = imageTagFunction(whiteRookUrl)
+                        square.innerHTML = image
+                        div.remove()
+                    })
+                    knightImage.addEventListener('click', () =>{
+                        console.log('you clicked the knight')
+                        let image = imageTagFunction(whiteKnightUrl)
+                        square.innerHTML = image
+                        div.remove()
+                    })
+                    queenImage.src = whiteQueenUrl
+                    rookImage.src = whiteRookUrl
+                    knightImage.src = whiteKnightUrl
+                    queenImage.style.width = '10%'
+                    rookImage.style.width = '10%'
+                    knightImage.style.width = '10%'
+                    div.appendChild(sentence)
+                    div.appendChild(queenImage)
+                    div.appendChild(rookImage)
+                    div.appendChild(knightImage)
+                    div.classList.add('row')
+                    optionsBox.appendChild(div)
+
+                }
+                else if(pieceToMove.includes(blackPawnUrl) && row === 8){
+                    // console.log('you made it!')
+                    let div = document.createElement('div')
+                    let sentence = document.createElement('div')
+                    let queenImage = document.createElement('img')
+                    let rookImage = document.createElement('img')
+                    let knightImage = document.createElement('img')
+
+                    div.classList.add('justify-content-center')
+                    sentence.classList.add('row')
+                    sentence.classList.add('justify-content-center')
+                    sentence.innerText = 'Select a piece to upgrade your pawn.'
+                    queenImage.addEventListener('click', () =>{
+                        let image = imageTagFunction(blackQueenUrl)
+                        square.innerHTML = image
+                        div.remove()
+                    })
+                    rookImage.addEventListener('click', () =>{
+                        console.log('you clicked the rook')
+                        let image = imageTagFunction(blackRookUrl)
+                        square.innerHTML = image
+                        div.remove()
+                    })
+                    knightImage.addEventListener('click', () =>{
+                        console.log('you clicked the knight')
+                        let image = imageTagFunction(blackKnightUrl)
+                        square.innerHTML = image
+                        div.remove()
+                    })
+                    queenImage.src = blackQueenUrl
+                    rookImage.src = blackRookUrl
+                    knightImage.src = blackKnightUrl
+                    queenImage.style.width = '10%'
+                    rookImage.style.width = '10%'
+                    knightImage.style.width = '10%'
+                    div.appendChild(sentence)
+                    div.appendChild(queenImage)
+                    div.appendChild(rookImage)
+                    div.appendChild(knightImage)
+                    div.classList.add('row')
+                    optionsBox.appendChild(div)
+
+                }
                 square.innerHTML = pieceToMove
                 pieceSelected = false
                 revertBorder(tileMovedFrom)
@@ -69,7 +166,6 @@ function addClickEvent(rowOfSquares){
                 square.style.border = ''
                 pieceToMove = ''
             }
-            // console.log(pieceToMove)
         })
     }
     return 'completed'
@@ -83,12 +179,13 @@ function revertBorder(tileNum){
 function revertPossibleMoveBorders(list){
     for(let square of list){
         let tile = document.querySelector(`#square-${square}`)
-        // console.log(tile)
         tile.style.border = ''
     }
 }
 
-function showPossibleMoves(list){
+function showPossibleMoves(list, currentTile){
+    let currentSquare = document.querySelector(`#square-${currentTile}`)
+    // console.log(currentSquare)
     for(let num of list){
         num = String(num)
         let square = document.querySelector(`#square-${num}`)
@@ -128,10 +225,8 @@ function findBishopPossiblesSquares(currentTile){
     let column = findColumn(currentTile)
     let currentSquare = currentTile
     let currentColumn = column
-    // console.log(currentTile)
     if(row === 1){
         while(currentColumn < 8){
-            // console.log(currentSquare + 9)
             possibleSquares.push(currentSquare += 9)
             currentColumn += 1
         }
@@ -156,8 +251,6 @@ function findBishopPossiblesSquares(currentTile){
     }
     else {
         let currentRow = row
-        // console.log(currentSquare)
-        // console.log(currentColumn)
         while(currentColumn < 8 && currentRow < 8){
             possibleSquares.push(currentSquare += 9)
             currentRow += 1
@@ -206,56 +299,42 @@ function findRookPossibleSquares(currentTile){
         while(possibleSquares.length < 15){
             possibleSquares.push(possibleSquares[possibleSquares.length - 1] + 8)
         }
-        // let squaresWithCurrentSquare = possibleSquares.filter(num => num !== currentTile)
-        // possibleSquares.concat(squaresWithCurrentSquare)
     }
     else if(column === 2){
         possibleSquares.push(2)
         while(possibleSquares.length < 15){
             possibleSquares.push(possibleSquares[possibleSquares.length - 1] + 8)
         }
-        // let squaresWithCurrentSquare = possibleSquares.filter(num => num !== currentTile)
-        // possibleSquares.concat(squaresWithCurrentSquare)
     }
     else if(column === 3){
         possibleSquares.push(3)
         while(possibleSquares.length < 15){
             possibleSquares.push(possibleSquares[possibleSquares.length - 1] + 8)
         }
-        // let squaresWithCurrentSquare = possibleSquares.filter(num => num !== currentTile)
-        // possibleSquares.concat(squaresWithCurrentSquare)
     }
     else if(column === 4){
         possibleSquares.push(4)
         while(possibleSquares.length < 15){
             possibleSquares.push(possibleSquares[possibleSquares.length - 1] + 8)
         }
-        // let squaresWithCurrentSquare = possibleSquares.filter(num => num !== currentTile)
-        // possibleSquares.concat(squaresWithCurrentSquare)
     }
     else if(column === 5){
         possibleSquares.push(5)
         while(possibleSquares.length < 15){
             possibleSquares.push(possibleSquares[possibleSquares.length - 1] + 8)
         }
-        // let squaresWithCurrentSquare = possibleSquares.filter(num => num !== currentTile)
-        // possibleSquares.concat(squaresWithCurrentSquare)
     }
     else if(column === 6){
         possibleSquares.push(6)
         while(possibleSquares.length < 15){
             possibleSquares.push(possibleSquares[possibleSquares.length - 1] + 8)
         }
-        // let squaresWithCurrentSquare = possibleSquares.filter(num => num !== currentTile)
-        // possibleSquares.concat(squaresWithCurrentSquare)
     }
     else if(column === 7){
         possibleSquares.push(7)
         while(possibleSquares.length < 15){
             possibleSquares.push(possibleSquares[possibleSquares.length - 1] + 8)
         }
-        // let squaresWithCurrentSquare = possibleSquares.filter(num => num !== currentTile)
-        // possibleSquares.concat(squaresWithCurrentSquare)
     }
     else if(column === 8){
         possibleSquares.push(8)
@@ -274,38 +353,99 @@ function findPossibleMoves(tileNum){
         possibleSquares = []
         let currentTile = Number(tileNum.split('-')[1])
         if(currentTile >= 49){
-            possibleSquares.push(currentTile - 8)
-            possibleSquares.push(currentTile - 16)
-            showPossibleMoves(possibleSquares)
+            
+            let possibleMove = document.querySelector(`#square-${currentTile - 8}`)
+            let possibleMove2 = document.querySelector(`#square-${currentTile - 16}`)
+            let possibleMove3 = document.querySelector(`#square-${currentTile - 7}`)
+            let possibleMove4 = document.querySelector(`#square-${currentTile - 9}`)
+            
+            if(!possibleMove.innerHTML.includes('img')){
+                possibleSquares.push(currentTile - 8)
+            }
+            if(!possibleMove2.innerHTML.includes('img') && possibleSquares.length > 0){
+                possibleSquares.push(currentTile - 16)
+            }
+            if(possibleMove3.innerHTML.includes('img')){
+                possibleSquares.push(currentTile - 7)
+            }
+            if(possibleMove4.innerHTML.includes('img')){
+                possibleSquares.push(currentTile - 9)
+            }
+
+            showPossibleMoves(possibleSquares, currentTile)
         }
         else {
-            possibleSquares.push(currentTile - 8)
-            showPossibleMoves(possibleSquares)
+            let possibleMove = document.querySelector(`#square-${currentTile - 8}`)
+            let possibleMove2 = document.querySelector(`#square-${currentTile - 9}`)
+            let possibleMove3 = document.querySelector(`#square-${currentTile - 7}`)
+            
+            if(!possibleMove.innerHTML.includes('img')){
+                possibleSquares.push(currentTile - 8)
+            }
+            if(possibleMove2.innerHTML.includes('img')){
+                possibleSquares.push(currentTile - 9)
+            }
+            if(possibleMove3.innerHTML.includes('img')){
+                possibleSquares.push(currentTile - 7)
+            }
+            showPossibleMoves(possibleSquares, currentTile)
         }
     }
     else if(pieceToMove.includes(blackPawnUrl)){
         possibleSquares = []
         let currentTile = Number(tileNum.split('-')[1])  
         if(currentTile <= 16){
-            possibleSquares.push(currentTile + 8)
-            possibleSquares.push(currentTile + 16)
-            showPossibleMoves(possibleSquares)
+            let possibleMove = document.querySelector(`#square-${currentTile + 8}`)
+            let possibleMove2 = document.querySelector(`#square-${currentTile + 16}`)
+            let possibleMove3 = document.querySelector(`#square-${currentTile + 7}`)
+            let possibleMove4 = document.querySelector(`#square-${currentTile + 9}`)
+            let row = findRow(currentTile)
+
+            if(row > 2){
+                if(!possibleMove.innerHTML.includes('img')){
+                    possibleSquares.push(currentTile + 8)
+                }
+                if(!possibleMove2.innerHTML.includes('img') && possibleSquares.length > 0){
+                    possibleSquares.push(currentTile + 16)
+                }
+                if(possibleMove3.innerHTML.includes('img')){
+                    possibleSquares.push(currentTile + 7)
+                }
+                if(possibleMove4.innerHTML.includes('img')){
+                    possibleSquares.push(currentTile + 9)
+                }
+                showPossibleMoves(possibleSquares, currentTile)
+            }
+            else{
+                console.log('you are now in row 2')
+            }
+            
         }
         else {
-            possibleSquares.push(currentTile + 8)
-            showPossibleMoves(possibleSquares)
+            let possibleMove = document.querySelector(`#square-${currentTile + 8}`)
+            let possibleMove2 = document.querySelector(`#square-${currentTile + 9}`)
+            let possibleMove3 = document.querySelector(`#square-${currentTile + 7}`)
+            if(!possibleMove.innerHTML.includes('img')){
+                possibleSquares.push(currentTile + 8)
+            }
+            if(possibleMove2.innerHTML.includes('img')){
+                possibleSquares.push(currentTile + 9)
+            }
+            if(possibleMove3.innerHTML.includes('img')){
+                possibleSquares.push(currentTile + 7)
+            }
+            showPossibleMoves(possibleSquares, currentTile)
         }
     }
     else if(pieceToMove.includes(whiteRookUrl) || pieceToMove.includes(blackRookUrl)){
         let currentTile = Number(tileNum.split('-')[1])
-        showPossibleMoves(findRookPossibleSquares(currentTile))
+        showPossibleMoves(findRookPossibleSquares(currentTile), currentTile)
     }
     else if(pieceToMove.includes(whiteKnightUrl) || pieceToMove.includes(blackKnightUrl)){
         possibleSquares = []
         let currentTile = Number(tileNum.split('-')[1])
         let row = findRow(currentTile)
         let column = findColumn(currentTile)
-        // console.log(currentTile)
         if(column <= 6 && column >= 3){
             if(row <= 6 && row >= 3){
                 possibleSquares.push(currentTile - 17)
@@ -349,10 +489,8 @@ function findPossibleMoves(tileNum){
                     possibleSquares.push(currentTile - 17)
                 }
             }
-            showPossibleMoves(possibleSquares)
         }
         else if(column === 2 || column === 7){
-            // console.log('column 2 or 7')
             if(row <= 6 && row >= 3){
                 if(column ===  2){
                     possibleSquares.push(currentTile - 17)
@@ -370,7 +508,7 @@ function findPossibleMoves(tileNum){
                     possibleSquares.push(currentTile + 15)
                     possibleSquares.push(currentTile + 17)
                 }
-                showPossibleMoves(possibleSquares)
+
             } 
             else if(row === 2 || row === 7){
                 if(column === 2){
@@ -401,7 +539,7 @@ function findPossibleMoves(tileNum){
                         possibleSquares.push(currentTile + 6)
                     }
                 }
-                showPossibleMoves(possibleSquares)
+
             }
             else if(row === 1 || row === 8){
                 if(column === 2){
@@ -429,7 +567,7 @@ function findPossibleMoves(tileNum){
                     }
 
                 }
-                showPossibleMoves(possibleSquares)
+
             }
         }
         else if(column === 1 || column === 8){
@@ -475,7 +613,6 @@ function findPossibleMoves(tileNum){
                 }
             }
             else if(row === 1 || row === 8){
-                // console.log('row 1 or 8')
                 if(column === 1){
                     if(row === 1){
                         possibleSquares.push(currentTile + 17)
@@ -492,7 +629,6 @@ function findPossibleMoves(tileNum){
                         possibleSquares.push(currentTile + 6)
                     }
                     else{
-                        // console.log('correct')
                         possibleSquares.push(currentTile - 17)
                         possibleSquares.push(currentTile - 10)
                     }
@@ -500,19 +636,20 @@ function findPossibleMoves(tileNum){
                 }
             }
         }
-        showPossibleMoves(possibleSquares)
+        // console.log(currentTile)
+        showPossibleMoves(possibleSquares, currentTile)
     }
     else if(pieceToMove.includes(whiteBishopUrl) || pieceToMove.includes(blackBishopUrl)){
         let currentTile = Number(tileNum.split('-')[1])
         findBishopPossiblesSquares(currentTile)
-        showPossibleMoves(possibleSquares)
+        showPossibleMoves(possibleSquares, currentTile)
     }
     else if(pieceToMove.includes(whiteQueenUrl) || pieceToMove.includes(blackQueenUrl)){
         let currentTile = Number(tileNum.split('-')[1])
         let rookMoves = findRookPossibleSquares(currentTile)
         let bishopMoves = findBishopPossiblesSquares(currentTile)
         possibleSquares = bishopMoves.concat(rookMoves)
-        showPossibleMoves(possibleSquares)
+        showPossibleMoves(possibleSquares, currentTile)
     }
     else if(pieceToMove.includes(whiteKingUrl) || pieceToMove.includes(blackKingUrl)){
         possibleSquares = []
@@ -583,7 +720,7 @@ function findPossibleMoves(tileNum){
                 possibleSquares.push(currentTile + 9)
             }
         }
-        showPossibleMoves(possibleSquares)
+        showPossibleMoves(possibleSquares, currentTile)
     }
 }
 
